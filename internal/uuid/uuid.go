87912c5bb0215cbdebe0712c827b1de152f8fa87
noaway <7072547@qq.com>
2016-11-04 14:20:43 +0800

    no message

--- internal/uuid/uuid.go
+++ internal/uuid/uuid.go
@@ -145,7 +145,7 @@ func (u UUID) String() string {
 		u[:4], u[4:6], u[6:8], u[8:10], u[10:])
 }
 
-func (U UUID) StringTrim() string {
+func (u UUID) StringTrim() string {
 	return fmt.Sprintf("%x%x%x%x%x",
 		u[:4], u[4:6], u[6:8], u[8:10], u[10:])
 }
