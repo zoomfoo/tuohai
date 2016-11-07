ed5dd4b300d81995ee9111c1a29d7378dca16766
noaway <7072547@qq.com>
2016-11-07 11:39:59 +0800

    no message

--- im_api/options.go
+++ im_api/options.go
@@ -29,7 +29,7 @@ type Options struct {
 func NewOptions() *Options {
 	Opts = &Options{
 		ID:          os.Getpid(),
-		HTTPAddress: "0.0.0.0:8081",
+		HTTPAddress: "0.0.0.0:10011",
 
 		DbHost: "120.27.45.244:3306",
 		DbUser: "root",
